9d62c7e8e9e8dab8b8b217bb61435f665219156f
Muhammad Fajar Budi Dharmawan <mfbdofficial1@gmail.com>
2025-02-27 11:53:23 +0700

    add(logic-training-1): No Zeros for Heroes, Volume of a Cuboid, Remove Anchor from URL

--- logic-training-1.js
+++ logic-training-1.js
@@ -70,4 +70,160 @@ function persistence(num) {
         num = num.toString().split('').reduce((acc, current) => acc * current); //mirip pro solution 1
     }
     return i;
- }
+}
+
+//Codewars - No Zeros for Heroes
+//Numbers ending with zeros are boring. They might be fun in your world, but not here. Get rid of them. 
+//Only the ending ones.
+//1450 -> 145; 960000 -> 96; 1050   -> 105; -1050 -> -105; 0 -> 0
+function noBoringZeros(n) {
+    let nums = n.toString().split("");
+    if (nums.length == 1) {
+        return n;
+    }
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] == 0) {
+            nums.pop();
+        } else {
+            return parseInt(nums.join(""));
+        }
+    }
+}
+//Pro solution 1
+function noBoringZeros(n) {
+    while (n % 10 === 0 && n !== 0) {
+        n = n / 10
+    }
+    return n
+}
+//Pro solution 2
+function noBoringZeros(n) {
+    return +`${n}`.replace(/0*$/, "");
+}
+//Pro solution 3
+function noBoringZeros(n) {
+    return Number(String(n).replace(/0+$/,''));
+}
+
+//Codewars - Volume of a Cuboid
+//Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height 
+//of the cuboid. Write a function to help Bob with this calculation.
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        return length * width * height;
+    }
+}
+//Pro solution 1
+const Kata = {
+    getVolumeOfCuboid : (length, width, height) => length * width * height
+}
+
+//Codewars - Remove Anchor from URL
+//Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+//"www.codewars.com#about" --> "www.codewars.com"
+//"www.codewars.com?page=1" --> "www.codewars.com?page=1"
+function removeUrlAnchor(url){
+    let cleanURL = "";
+    for (let i = 0; i < url.length; i++) {
+        if (url[i] == "#") {
+            return cleanURL;
+        }
+        cleanURL = cleanURL + url[i];
+    }
+    return cleanURL;
+}
+//Pro solution 1
+function removeUrlAnchor(url){
+    return url.split('#')[0]; //split (pisahkan) string berdasarkan "#" lalu ambil yg pertama saja,
+} //yaitu index ke 0
+//Pro solution 2
+function removeUrlAnchor(url){
+    return url.replace(/#.*/gi, ""); //pakai regex, ganti bagian yg "#..." (ada anchor, juga karakter
+} //seterusnya) menjadi string kosong ''
+
+
+/*
+//Codewars - Check for factor
+//Factors are numbers you can multiply together to get another number.
+//2 and 3 are factors of 6 because: 2 * 3 = 6
+//You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor. You can
+//use the mod operator (%) in most languages to check for a remainder
+function checkForFactor (base, factor) {
+    return base % factor == 0 ? true : false;
+}
+//Pro solution 1
+function checkForFactor (base, factor) {
+    return base % factor === 0;
+}
+//Pro solution 2
+const checkForFactor = (base, factor) => !(base % factor);
+
+//Codewars - Get the Middle Character
+//You are going to be given a non-empty string. Your job is to return the middle character(s) of the 
+//string. If the string's length is odd, return the middle character. If the string's length is even, 
+//then return the middle 2 characters.
+function getMiddle(s) {
+    let middleIndex = Math.floor(s.length / 2); //floor membulatkan angka ke bawah
+    if (s.length % 2 == 0) {
+        return s[middleIndex - 1] + s[middleIndex]; 
+    } else {
+        return s[middleIndex];
+    }
+}
+//Pro solution 1
+function getMiddle(s) { 
+    return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1); //substr mengambil string 
+} //mulai dari urutan tertentu, lalu dengan jumlah karakter tertentu. Lalu ceil() membulatkan ke atas
+
+//Codewars - Build Tower
+//Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. 
+//A tower block is represented with "*" character. For example, a tower with 3 floors looks like this:
+//[
+//    "  *  ",
+//    " *** ", 
+//    "*****"
+//]
+function towerBuilder(nFloors) {
+    let arrPyramid = [];
+    let current = nFloors - 1;
+    for (let i = 0; i < nFloors; i++) {
+        let element = "";
+        for (let j = 0; j < nFloors; j++) {
+            if (j < current) {
+                element += " ";
+            } else {
+                element += "*";
+            }
+        }
+        for (let k = nFloors - 1; k > 0; k--) {
+            if (k > current) {
+                element += "*";
+            } else {
+                element += " ";
+            }
+        }
+        current = current - 1;
+        arrPyramid.push(element);
+    }
+    return arrPyramid
+}
+//Pro solution 1
+function towerBuilder(nFloors) {
+    var tower = [];
+    for (var i = 0; i < nFloors; i++) {
+      tower.push(" ".repeat(nFloors - i - 1) + "*".repeat((i * 2)+ 1) + " ".repeat(nFloors - i - 1));
+    }
+    return tower;
+}
+//Pro solution 2
+function towerBuilder(n) {
+    return Array.from({length: n}, function(v, k) {
+        const spaces = ' '.repeat(n - k - 1);
+        return spaces + '*'.repeat(k + k + 1) + spaces;
+    });
+}
+//Pro solution 3
+function towerBuilder(n) {
+    return [...Array(n)].map((_, i) => " ".repeat(n - 1 - i) + "*".repeat(i * 2 + 1) + " ".repeat(n - 1 -i))
+}
+*/
--- logic-training-1.php
+++ logic-training-1.php
@@ -63,3 +63,190 @@ function persistence(int $num): int {
     }
     return $count;
 }
+
+//Codewars - No Zeros for Heroes
+//Numbers ending with zeros are boring. They might be fun in your world, but not here. Get rid of them. 
+//Only the ending ones.
+//1450 -> 145; 960000 -> 96; 1050   -> 105; -1050 -> -105; 0 -> 0
+function noBoringZeros(int $n): int {
+    $nums = str_split(strval($n));
+    if(count($nums) == 1) {
+        return $n;
+    }
+    for($i = count($nums) - 1; $i >= 0; $i--) {
+        if($nums[$i] == 0) {
+            array_pop($nums);
+        } else {
+            return intval(implode("", $nums));
+        }
+    }
+    return $n;
+}
+//Pro solution 1
+function noBoringZeros(int $n): int {
+    return (int) rtrim($n,'0');
+}
+
+//Codewars - Volume of a Cuboid
+//Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height 
+//of the cuboid. Write a function to help Bob with this calculation.
+function get_volume_of_cuboid($length, $width, $height) {
+    return $length * $width * $height;
+}
+//Pro solution 1
+function get_volume_of_cuboid(...$arg) : int {
+    return array_product($arg);
+}
+
+//Codewars - Remove Anchor from URL
+//Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+//"www.codewars.com#about" --> "www.codewars.com"
+//"www.codewars.com?page=1" --> "www.codewars.com?page=1"
+function replaceAll($string) {
+    $cleanURL = "";
+    for ($i = 0; $i < strlen($string); $i++) {
+        if ($string[$i] == "#") {
+            return $cleanURL;
+        }
+        $cleanURL .= $string[$i];
+    }
+    return $cleanURL;
+}
+//Pro solution 1
+function replaceAll($string) {
+    return explode("#", $string)[0]; //explode (pisahkan) string berdasarkan "#" lalu ambil yg pertama
+} //saja, yaitu index ke 0
+//Pro solution 2
+function replaceAll(string $string) :string {
+    return preg_replace('/#.*$/', '', $string); //pakai regex, ganti bagian yg "#..." (ada anchor,
+} //juga karakter seterusnya) menjadi string kosong ''
+//Pro solution 3
+function replaceAll($s) {
+    return strtok($s, "#");
+}
+
+
+/*
+//Codewars - Check for factor
+//Factors are numbers you can multiply together to get another number.
+//2 and 3 are factors of 6 because: 2 * 3 = 6
+//You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor. You can
+//use the mod operator (%) in most languages to check for a remainder
+function checkForFactor($base, $factor) {
+    return $base % $factor == 0 ? true : false;
+}
+//Pro solution 1
+function checkForFactor($base, $factor) {
+    return $base % $factor === 0;
+}
+//Pro solution 2
+function checkForFactor($base, $factor) {
+    return !($base % $factor);
+}
+
+//Codewars - Get the Middle Character
+//You are going to be given a non-empty string. Your job is to return the middle character(s) of the 
+//string. If the string's length is odd, return the middle character. If the string's length is even, 
+//then return the middle 2 characters.
+function getMiddle($text) {
+    $middleIndex = floor(strlen($text) / 2); //floor membulatkan angka ke bawah
+    if (strlen($text) % 2 == 0) {
+        return $text[$middleIndex - 1] . $text[$middleIndex];
+    } else {
+        return $text[$middleIndex];
+    }
+}
+//Pro solution 1
+function getMiddle($text) {
+    $len = strlen($text);
+    if ($len % 2 === 0) {
+        return substr($text, $len / 2 - 1, 2); //substr mengambil string mulai dari urutan tertentu,
+    } //lalu dengan jumlah karakter tertentu
+    return substr($text, $len / 2, 1);
+}
+//Pro solution 2
+function getMiddle($text) {
+	$start = floor((strlen($text) - 1) / 2); //floor membulatkan angka ke  bawah
+    $len = strlen($text) % 2 ? 1 : 2; //kalo hasil % 2 = 0 maka menjadi false, kalo 1 maka menjadi true
+	return substr($text, $start, $len);
+}
+
+//Codewars - Build Tower
+//Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. 
+//A tower block is represented with "*" character. For example, a tower with 3 floors looks like this:
+//[
+//    "  *  ",
+//    " *** ", 
+//    "*****"
+//]
+function tower_builder(int $n): array {
+    $arrPyramid = [];
+    $current1 = $n - 1;
+    $current2 = 0; 
+    for($i = 0; $i < $n; $i++) { //perulangan untuk baris sesuai $n
+        $element = "";
+        for($j = 0; $j < $n; $j++) { //perulangan untuk string bintang di bagian kiri $n
+            if($j < $current1) {
+                $element .= " ";
+            } else {
+                $element .= "*";
+            }
+        }
+        for($k = 0; $k < $n - 1; $k++) { //perulangan untuk string bintang di bagian kanan $n - 1
+            if($k < $current2) {
+                $element .= "*";
+            } else {
+                $element .= " ";
+            }
+        }
+      $current1 = $current1 - 1;
+      $current2++;
+      $arrPyramid[] = $element;
+    }
+    return $arrPyramid;
+}
+//Model memakai 1 current counter dows
+function tower_builder(int $n): array {
+    $arrPyramid = [];
+    $current = $n - 1; //current pembanding memakai angka dari $n
+    for($i = 0; $i < $n; $i++) {
+      $element = "";
+        for($j = 0; $j < $n; $j++) {
+            if($j < $current) {
+                $element .= " ";
+            } else {
+                $element .= "*";
+            }
+        }
+        for($k = $n - 1; $k > 0; $k--) { //model for ini harus terbalik karena align bintang terbalik
+            if($k > $current) {
+                $element .= "*";
+            } else {
+                $element .= " ";
+            }
+        }
+        $current = $current - 1;
+        $arrPyramid[] = $element;
+    }
+    return $arrPyramid;
+}
+//Pro solution 1
+function tower_builder(int $n): array {
+    $pad = $n * 2 - 1;
+    $x = 1; 
+	$arr = [];
+    while ($n --> 0) {
+    	$arr[] = str_pad(str_repeat('*', $x), $pad, ' ', STR_PAD_BOTH);
+        $x += 2;
+    }
+    return $arr;
+}
+//Pro solution 2
+function tower_builder(int $n): array {
+    $result = array();
+    for($i = 1; $i <= $n; $i++) {
+        $result[] = str_repeat(' ', $n-$i) . str_repeat('*', ($i-1)*2+1) . str_repeat(' ', $n-$i);
+    }
+    return $result;
+}
+*/
